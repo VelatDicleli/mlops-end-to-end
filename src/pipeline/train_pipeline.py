007efed8d447c98037211bf71f85ae44997c450b
Velat Dicleli <diclelivelat@gmail.com>
2025-07-10 00:26:41 +0300

    k8s folder added

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -3,22 +3,26 @@ from src.workflow.model_trainer import ModelTrainerConfig, ModelTrainer
 from src.workflow.data_ingestion import DataIngestion, DataIngestionConfig
 from src.logger import logging
 
+def main():
+
+    config = DataIngestionConfig()
+    data_ingestion = DataIngestion(config)
+
+    train_file, test_file = data_ingestion.get_data()
+
+    logging.info(f"Train file: {train_file}")
+    logging.info(f"Test file: {test_file}")
 
 
-config = DataIngestionConfig()
-data_ingestion = DataIngestion(config)
+    data_transformation_config = DataTransformationConfig()
+    data_transformation = DataTransformation(data_transformation_config)
+    train_array, test_array = data_transformation.get_transformed_data(train_file, test_file)
 
-train_file, test_file = data_ingestion.get_data()
+    model_trainer_config = ModelTrainerConfig()
+    model_trainer = ModelTrainer(model_trainer_config)
+    model_trainer.train_model(train_array, test_array)
+    logging.info("Model training completed successfully.")
 
-logging.info(f"Train file: {train_file}")
-logging.info(f"Test file: {test_file}")
 
-
-data_transformation_config = DataTransformationConfig()
-data_transformation = DataTransformation(data_transformation_config)
-train_array, test_array = data_transformation.get_transformed_data(train_file, test_file)
-
-model_trainer_config = ModelTrainerConfig()
-model_trainer = ModelTrainer(model_trainer_config)
-model_trainer.train_model(train_array, test_array)
-logging.info("Model training completed successfully.")
+if __name__ == "__main__":
+    main()
